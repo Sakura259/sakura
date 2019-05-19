c0e493c0d218209c3d64a9a2f6966b090f6ac025
Sakura259 <1582072981@qq.com>
2019-05-19 18:05:07 +0800

    heapSort

--- src/com/alogrithm/sort/HeapSort.java
+++ src/com/alogrithm/sort/HeapSort.java
@@ -1,5 +1,7 @@
 package com.alogrithm.sort;
 
+import java.util.Arrays;
+
 /**
  * Created by Sakura on 2019/5/19.
  * 堆排序：利用堆结构实现的排序算法（不稳定）
@@ -17,15 +19,15 @@ public class HeapSort {
      * */
     public static void heapSort(int[] arr) {
         //第一次初始化大根堆，从最后一个有子节点的根节点开始往上调整最大堆
-        for (int i = arr.length/2; i >= 0; i--) {
+        for (int i = arr.length/2-1; i >= 0; i--) {
             adjustHeap(arr,i,arr.length);
         }
+        System.out.println(Arrays.toString(arr));
         //从顶点开始往下调整大根堆
         for (int i = arr.length-1; i > 0; i--) {
             int temp = arr[0]; //最后一个叶子节点与根节点交换，使得最大的数在最后
             arr[0] = arr[i];
             arr[i] = temp;
-
             adjustHeap(arr, 0, i);
         }
     }
@@ -35,7 +37,7 @@ public class HeapSort {
             if(child+1<length&&arr[child]<arr[child+1]){//判断左右叶子节点哪个最大
                 child++;
             }
-            if(arr[child]>temp){//子节点大于右节点，则交换
+            if(arr[child]>temp){//子节点大于父节点，则交换
                 arr[parent] = arr[child];
                 parent = child;
             }
